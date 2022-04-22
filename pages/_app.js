8d8a63b1df8e0235e5632877afdd2b1b50f2afde
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-04-22 16:32:37 +0200

    Added custom _document for styled-components ssr

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,10 @@
 import { ThemeProvider } from "styled-components"
 import { GlobalStyle, Theme, SelectStyle } from "../styles/GlobalStyle"
-import Head from 'next/head'
-import Select from 'react-select'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href={"https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"} rel="stylesheet" />
-      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
