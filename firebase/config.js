e8ee8dd814af8712b467e4c442809a0548e1658b
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-04-22 16:27:55 +0200

    Added local env firebase variables

--- firebase/config.js
+++ firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDpJk92bNWbINMAPsXBRb8IUZ4Kf68YqpU",
-    authDomain: "immobilier-2.firebaseapp.com",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: "immobilier-2",
     storageBucket: "immobilier-2.appspot.com",
     messagingSenderId: "525209019231",
-    appId: "1:525209019231:web:17eded29f216674e128cea"
+    appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
